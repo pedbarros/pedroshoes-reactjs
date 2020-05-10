58e5c06768fee145c43b46e7f378ff9640591f91
Pedro Barros <pedro.barros@herospark.com>
2020-05-10 14:05:54 -0300

    adicionando estilos globais a aplicação

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 // import { Container } from './styles';
 
 function Cart() {
-  return <div />;
+  return <div>Cart</div>;
 }
 
 export default Cart;
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,10 @@
+import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Cart from './pages/Cart';
 
-export default function Routers() {
+export default function Routes() {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
